0005fb0f5032a6aaf13e6b2e78287013f1b18085
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-28 14:37:12 +0100

    ternaries - task

--- modern_javascript_exercises/ternaries/ternaries.js
+++ modern_javascript_exercises/ternaries/ternaries.js
@@ -1,7 +1,3 @@
 // Instructions can be found in ternaries.md
 
-export function sayHello(name) {
-  if (name === undefined) {
-    return "Hello you!";
-  } else return "Hello, " + name + "!";
-}
+export const sayHello = (name) => `Hello${name ? `, ${name}` : ' you'}!`;
