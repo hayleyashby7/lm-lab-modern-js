b1f74f3680f66e7a90547b106a7d77c2e7083cfa
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-28 14:12:34 +0100

    destructuring_arrays - task

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,5 @@
 // Instructions can be found in destructuring_arrays.md
 
 export function sequence(order) {
-  const first = order[0];
-  const second = order[1];
-  const third = order[2];
-
-  return first + ", " + second + ", " + third;
+	return `${order[0]}, ${order[1]}, ${order[2]}`;
 }
