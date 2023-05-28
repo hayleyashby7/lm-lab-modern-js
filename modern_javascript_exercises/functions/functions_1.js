b342460926098b6c0725397c3f1e45390f33da81
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-28 13:21:40 +0100

    functions_1 - function variable

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -5,10 +5,10 @@
 // Note how it uses the "function" keyword at the beginning of it's declaration
 // This is the original way of writing functions in JavaScript
 
-const people = ["Harry", "Hermione", "Ron"];
+const people = ['Harry', 'Hermione', 'Ron'];
 
 function logPerson(person) {
-  console.log("The person is " + person);
+	console.log('The person is ' + person);
 }
 
 people.forEach(logPerson);
@@ -16,7 +16,11 @@ people.forEach(logPerson);
 // Function in a Variable
 // Store the logPerson() function in a variable called performLogPerson
 
-// Your code here
+const performLogPerson = function (person) {
+	console.log('The person is ' + person);
+};
+
+people.forEach(logPerson);
 
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
@@ -29,3 +33,8 @@ people.forEach(logPerson);
 people.forEach(logPerson);
 
 // In JavaScript there are lots of ways to write functions. There is no right or wrong way, but arrow functions are the most modern. 🙂
+
+/* 👉 What is the difference between all these functions? Can you explain what the differences are?
+
+
+*/
