01dd0cca8d7a59dff1abba6e1ff6e227b9f8b089
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-28 14:15:49 +0100

    destructuring_objects - task

--- modern_javascript_exercises/destructuring_objects/destructuring_objects.js
+++ modern_javascript_exercises/destructuring_objects/destructuring_objects.js
@@ -1,9 +1,3 @@
 // Instructions can be found in destructuring_objects.md
 
-export function introduction(person) {
-  const name = person.name;
-  const city = person.city;
-  const pet = person.pet;
-
-  return "Hello my name is " + name + " and I live in " + city + " with " + pet;
-}
+export const introduction = ({ name, city, pet }) => `Hello my name is ${name} and I live in ${city} with ${pet}`;
