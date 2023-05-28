9baf7aafbeb51d2f247881ebdc782d5cb38631c8
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-28 13:37:18 +0100

    functions_1 - anonymous arrow function and explainations

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -34,11 +34,14 @@ people.forEach(arrowVersionOfLogPerson);
 // Anonymous Arrow Functions
 // Replace logPerson below with an Anonymous Arrow Function that does the same thing!
 
-people.forEach(logPerson);
+people.forEach((person) => console.log('The person is ' + person));
 
 // In JavaScript there are lots of ways to write functions. There is no right or wrong way, but arrow functions are the most modern. 🙂
 
 /* 👉 What is the difference between all these functions? Can you explain what the differences are?
 
+Functions in a variable - they are proper function objects which means they have access to arguements and 'this' constructs. They can construct instances of an object using new. They are also hoisted, which means they can be accessed in the file before their definition. However it doesn't have access to local variables and is slow because it is compiled at runtime.
+Arrow Functions - they don't have access to arguements or 'this' constructs, although you can use the spread operator as a kind of arguements alternative. You can't use them as methods, or to create objects with new like Functions and they are not hoisted. However they are faster than standard functions and help preserve scope because they don't need closures.
+Anonymouse Arrow Functions - very similar to Arrow Functions but without a name. Because they are anonymous, they are used where they are placed, making them good for quick oneliners, callbacks in array methods etc.
 
 */
