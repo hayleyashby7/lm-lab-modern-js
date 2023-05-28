68b253233e228e7045495fa16eb57c8c40d7b6c1
Hayley Ashby <hayleyashby7@gmail.com>
2023-05-28 14:52:36 +0100

    Refactored longpasswords to use filter

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -1,9 +1,3 @@
 // Instructions can be found in imperative_vs_declarative.md
 
-export const longPasswords = (passwords) => {
-	const longPasswords = [];
-
-	passwords.forEach((password) => (password.length >= 9 ? longPasswords.push(password) : null));
-
-	return longPasswords;
-};
+export const longPasswords = (passwords) => passwords.filter((password) => password.length >= 9);
